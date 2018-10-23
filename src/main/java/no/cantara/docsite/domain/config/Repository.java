a56eba28a2b51c5574f9e409a5e6ddda7faa921a
totto <totto@totto.org>
2018-10-23 15:27:53 +0200

    moved template organization and reponame to constants

--- src/main/java/no/cantara/docsite/domain/config/Repository.java
+++ src/main/java/no/cantara/docsite/domain/config/Repository.java
@@ -8,6 +8,9 @@ import java.util.Objects;
 
 public class Repository implements Serializable {
 
+    public static final String SCP_TEMPLATE_REPO_NAME = "ConfigService";
+    public static final String SCP_TEMPLATE_ORGANIZATION_NAME = "Cantara";
+
     private static final long serialVersionUID = 3135783516132571382L;
 
     public final CacheKey cacheKey;
@@ -19,13 +22,13 @@ public class Repository implements Serializable {
     public final String rawRepoURL;
     public final String readmeURL;
     public final String contentsURL;
-    public String jenkinsURL = "https://jenkins.capraconsulting.no/buildStatus/icon?job=ConfigService";
-    public String groupCommit = "https://img.shields.io/github/last-commit/Cantara/ConfigService.svg";
+    public String jenkinsURL = "https://jenkins.capraconsulting.no/buildStatus/icon?job=" + SCP_TEMPLATE_REPO_NAME;
+    public String groupCommit = "https://img.shields.io/github/last-commit/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME + ".svg";
     public String groupStatus = "unknown";
-    public String groupRelease = "https://img.shields.io/github/tag/Cantara/ConfigService.svg";
+    public String groupRelease = "https://img.shields.io/github/tag/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME + ".svg";
     public String no_repos = "https://img.shields.io/badge/repos-5-blue.svg";
-    public String snykIOUrl = "https://snyk.io/test/github/Cantara/ConfigService/badge.svg";
-    public String snyktestIOUrl = "https://snyk.io/test/github/Cantara/ConfigService";
+    public String snykIOUrl = "https://snyk.io/test/github/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME + "/badge.svg";
+    public String snyktestIOUrl = "https://snyk.io/test/github/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME;
 
 
     Repository(CacheKey cacheKey, String id, String name, String defaultGroupRepo, String description, String repoURL, String rawRepoURL, String readmeURL, String contentsURL) {
@@ -39,13 +42,13 @@ public class Repository implements Serializable {
         this.readmeURL = readmeURL;
         this.contentsURL = contentsURL;
         if (defaultGroupRepo != null) {
-            if (snykIOUrl != null && snykIOUrl.contains("ConfigService")) {
-                this.snykIOUrl = snykIOUrl.replace("ConfigService", defaultGroupRepo);
+            if (snykIOUrl != null && snykIOUrl.contains(SCP_TEMPLATE_REPO_NAME)) {
+                this.snykIOUrl = snykIOUrl.replace(SCP_TEMPLATE_REPO_NAME, defaultGroupRepo);
             }
-            this.snyktestIOUrl = snyktestIOUrl.replace("ConfigService", defaultGroupRepo);
-            this.jenkinsURL = jenkinsURL.replace("ConfigService", defaultGroupRepo);
-            this.groupRelease = groupRelease.replace("ConfigService", defaultGroupRepo);
-            this.groupCommit = groupCommit.replace("ConfigService", defaultGroupRepo);
+            this.snyktestIOUrl = snyktestIOUrl.replace(SCP_TEMPLATE_REPO_NAME, defaultGroupRepo);
+            this.jenkinsURL = jenkinsURL.replace(SCP_TEMPLATE_REPO_NAME, defaultGroupRepo);
+            this.groupRelease = groupRelease.replace(SCP_TEMPLATE_REPO_NAME, defaultGroupRepo);
+            this.groupCommit = groupCommit.replace(SCP_TEMPLATE_REPO_NAME, defaultGroupRepo);
         }
     }
 
--- src/main/java/no/cantara/docsite/domain/view/DashboardModel.java
+++ src/main/java/no/cantara/docsite/domain/view/DashboardModel.java
@@ -3,6 +3,9 @@ package no.cantara.docsite.domain.view;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import static no.cantara.docsite.domain.config.Repository.SCP_TEMPLATE_ORGANIZATION_NAME;
+import static no.cantara.docsite.domain.config.Repository.SCP_TEMPLATE_REPO_NAME;
+
 public class DashboardModel {
 
     public final Set<Group> groups = new LinkedHashSet<>();
@@ -23,12 +26,12 @@ public class DashboardModel {
         public final String readmeURI;
         public final String cardURI;
         public final String jenkinsURL;
-        public String groupCommit = "https://img.shields.io/github/last-commit/Cantara/ConfigService.svg";
+        public String groupCommit = "https://img.shields.io/github/last-commit/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME + ".svg";
         public String groupStatus = "unknown";
-        public String groupRelease = "https://img.shields.io/github/tag/Cantara/ConfigService.svg";
+        public String groupRelease = "https://img.shields.io/github/tag/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME + ".svg";
         public String no_repos = "https://img.shields.io/badge/repos-5-blue.svg";
-        public String snykIOUrl = "https://snyk.io/test/github/Cantara/ConfigService/badge.svg";
-        public String snyktestIOUrl = "https://snyk.io/test/github/Cantara/ConfigService";
+        public String snykIOUrl = "https://snyk.io/test/github/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME + "/badge.svg";
+        public String snyktestIOUrl = "https://snyk.io/test/github/" + SCP_TEMPLATE_ORGANIZATION_NAME + "/" + SCP_TEMPLATE_REPO_NAME;
 
         public Set<Activity> activity = new LinkedHashSet<>();
 
@@ -47,10 +50,10 @@ public class DashboardModel {
             if (repoName.contains("*") && defaultGroupRepo != null) {
                 repoName = defaultGroupRepo;
             }
-            this.snykIOUrl = snykIOUrlx.replaceAll("ConfigService", repoName);
-            this.snyktestIOUrl = snyktestIOUrl.replace("ConfigService", repoName);
-            this.groupRelease = groupRelease.replace("ConfigService", repoName);
-            this.groupCommit = groupCommit.replace("ConfigService", repoName);
+            this.snykIOUrl = snykIOUrlx.replaceAll(SCP_TEMPLATE_REPO_NAME, repoName);
+            this.snyktestIOUrl = snyktestIOUrl.replace(SCP_TEMPLATE_REPO_NAME, repoName);
+            this.groupRelease = groupRelease.replace(SCP_TEMPLATE_REPO_NAME, repoName);
+            this.groupCommit = groupCommit.replace(SCP_TEMPLATE_REPO_NAME, repoName);
             //this.snykIOUrl = snykIOUrl;
         }
 
