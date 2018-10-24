ef9f0f9a1568595bf72c0c51098d779eba32cfcc
oranheim <oranheim@gmail.com>
2018-10-24 19:56:30 +0200

    Template improvements

--- src/test/java/no/cantara/docsite/cache/JCacheTest.java
+++ src/test/java/no/cantara/docsite/cache/JCacheTest.java
@@ -1,15 +1,24 @@
 package no.cantara.docsite.cache;
 
 import no.cantara.docsite.domain.github.contents.RepositoryContents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
+import java.util.Spliterator;
 
 //@Listeners(TestServerListener.class)
 public class JCacheTest {
 
-//    @Inject
+    private static final Logger LOG = LoggerFactory.getLogger(JCacheTest.class);
+
+    //    @Inject
     CacheStore cacheStore;
 
     @Ignore
@@ -20,4 +29,23 @@ public class JCacheTest {
 //        cache.put("key2", "value2");
     }
 
+    @Test
+    public void testName() {
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+
+        MutableConfiguration<String, String> cacheConfig = new MutableConfiguration<>();
+
+        Cache<String, String> cache = cacheManager.createCache("test", cacheConfig);
+
+        cache.put("1", "a");;
+        cache.put("2", "b");;
+
+        Spliterator<Cache.Entry<String, String>> spliterator = cache.spliterator();
+
+        int x = spliterator.getComparator().compare(null, null);
+
+        LOG.trace("{}", cache.spliterator());
+
+    }
 }
