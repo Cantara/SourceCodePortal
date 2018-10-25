0e5ab5dc5afd2a71759a53fa841901df6c6987fa
oranheim <oranheim@gmail.com>
2018-10-25 11:14:32 +0200

    Print options

--- src/main/java/no/cantara/docsite/oauth/ObtainGitHubAccessToken.java
+++ src/main/java/no/cantara/docsite/oauth/ObtainGitHubAccessToken.java
@@ -57,7 +57,7 @@ public class ObtainGitHubAccessToken implements Closeable {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        LOG.trace("ChromeOptions: {}", options.asMap());
+        LOG.info("ChromeOptions: {}", options.asMap());
         driver = new ChromeDriver(options);
     }
 
