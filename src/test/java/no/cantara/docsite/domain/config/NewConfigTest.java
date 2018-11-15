ff40b182768898733c40b769d638f2307a417fb4
oranheim <oranheim@gmail.com>
2018-11-15 20:50:13 +0100

    Added new repo builder

---
+++ src/test/java/no/cantara/docsite/domain/config/NewConfigTest.java
@@ -0,0 +1,54 @@
+package no.cantara.docsite.domain.config;
+
+import no.cantara.docsite.json.JsonDocumentTraversal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class NewConfigTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RepoConfigTest.class);
+
+    @Ignore
+    @Test
+    public void testJsonDocumentTraversal() throws IOException {
+        JsonObject jsonDocument;
+        try (InputStream in = ClassLoader.getSystemResourceAsStream("conf/new_repo_config.json")) {
+            try (JsonReader reader = Json.createReader(new InputStreamReader(in))) {
+                jsonDocument = reader.readObject();
+            }
+        }
+
+        JsonDocumentTraversal.walk(jsonDocument, (ancestors, jte) -> LOG.trace("{}[{}] {} isArray: {} - isNewSibling: {} - isArrayElement: {} -> {}: {} -> {}",
+                ancestors.stream().map(m -> " ").collect(Collectors.joining()), jte.depth(ancestors), jte.value.getValueType(),
+                jte.isArray(), jte.isNewSibling(), jte.isArrayElement(),
+                jte.path(ancestors), jte.key, jte.value));
+    }
+
+    @Test
+    public void testConfigBuilder() {
+        NewRepoConfig.Builder builder = NewRepoConfig.newBuilder("Title").withProvider(NewRepoConfig.newScmBuilder(NewRepoConfig.ScmProvider.GITHUB)
+                .matchRepository("SourceCodePortal*", "master")
+                .matchRepository("Whydah*", "master"))
+                .withRepositoryOverride(NewRepoConfig.newScmRepositoryOverrideBuilder()
+                        .repositoryId("SourceCodePortal")
+                        .displayName("Source Code Portal")
+                        .description("The SCP gathers")
+                        .repository("github:Cantara/SourceCodePortal*")
+                        .branch("master")
+                        .withExternal(NewRepoConfig.newJenkinsBuilder().prefix("Cantara-"))
+                        .withExternal(NewRepoConfig.newSnykBuilder().prefix("Cantara"))
+                )
+                ;
+    }
+
+}
