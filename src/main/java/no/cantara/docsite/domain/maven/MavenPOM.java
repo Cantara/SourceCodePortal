22f586cc77db8a0001f47f2be9900c82a0ffcce1
Jose Diaz <jose.diaz@lendistry.com>
2022-05-30 17:04:51 -0500

    downgrade some libraries to get project running successfully

--- src/main/java/no/cantara/docsite/domain/maven/MavenPOM.java
+++ src/main/java/no/cantara/docsite/domain/maven/MavenPOM.java
@@ -1,11 +1,12 @@
 package no.cantara.docsite.domain.maven;
 
-import no.cantara.docsite.json.JsonbFactory;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import no.cantara.docsite.json.JsonbFactory;
+
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
